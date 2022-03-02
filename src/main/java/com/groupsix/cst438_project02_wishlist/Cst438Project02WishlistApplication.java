f319efdb8712c5d4c6cd98c21a7fc05476fa90df
hernandez <alfhernandez@csumb.edu>
2022-03-02 13:35:42 -0800

    Database connection, edited entities, add queries to repository

--- src/main/java/com/groupsix/cst438_project02_wishlist/Cst438Project02WishlistApplication.java
+++ src/main/java/com/groupsix/cst438_project02_wishlist/Cst438Project02WishlistApplication.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
